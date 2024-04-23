30ff56021a4d553ad11c18f720bfa896c5b3cb52
Joao Vitor Caversan <joaovitorcaversan@gmail.com>
2024-04-23 01:50:44 -0300

    feat: apenas usuarios autenticados podem acessar o menu de admin/user

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,23 @@
-import { View, Text } from "react-native";
+import { View, Text, ActivityIndicator } from "react-native";
 import React from "react";
-import { Link } from "expo-router";
+import { Link, Redirect } from "expo-router";
 import Button from "../components/Button";
+import { useAuth } from "../providers/AuthProvider";
+import { supabase } from "../lib/supabase";
 
 const index = () => {
+  const { session, loading } = useAuth();
+
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+
+  if (!session) {
+    return <Redirect href={"/sign-in"} />;
+  }
+
+  console.log(session);
+
   return (
     <View style={{ flex: 1, justifyContent: "center", padding: 10 }}>
       <Link href={"/(user)"} asChild>
@@ -15,6 +29,8 @@ const index = () => {
       <Link href={"/sign-in"} asChild>
         <Button text="Login" />
       </Link>
+
+      <Button onPress={() => supabase.auth.signOut()} text="Sair" />
     </View>
   );
 };
